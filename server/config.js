6f9f1effb6e139ecf4427e2ba917234f19576fc8
Franks2695 <frankssanay@gmail.com>
2021-07-19 07:20:24 -0500

    URL de BDD en variable de entorno

--- server/config.js
+++ server/config.js
@@ -12,9 +12,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 let urlDB;
 
 if (process.env.NODE_ENV == "dev") {
-    urlDB = 'mongodb+srv://Sorel:violadores@viernes.2qyqm.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://Sorel:violadores@viernes.2qyqm.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.BDD_URL;
 }
 
 process.env.URLDB = urlDB;
